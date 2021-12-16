b0569ccaec47313618b67c408a6bdcf83790e9ee
Marek Legris <m.legris@freenet.de>
2021-12-16 08:59:30 +0100

    moved createModel to Model

--- src/scripts/Model.java
+++ src/scripts/Model.java
@@ -35,4 +35,23 @@ public class Model {
 
 		return image.getImageForDirection(direction);
 	}
+
+	public static Model createModel(String prefix) {
+		final var idleImages = new String[8];
+
+		for (var i = 0; i < 8; i++)
+			idleImages[i] = String.format("%s_%s_Idle0.png", prefix, i);
+
+		final var walkingAnimation = new IsometricImage[10];
+		final var images = new String[8];
+
+		for (var frame = 0; frame < 10; frame++) {
+			for (var i = 0; i < 8; i++)
+				images[i] = String.format("%s_%s_Run%s.png", prefix, i, frame);
+
+			walkingAnimation[frame] = new IsometricImage(images);
+		}
+
+		return new Model(new IsometricImage(idleImages), new Animation(walkingAnimation, 7));
+	}
 }
--- src/scripts/Player.java
+++ src/scripts/Player.java
@@ -3,25 +3,6 @@ package scripts;
 public class Player<I> extends CharacterObject<I> {
 
 	public Player() {
-		super(createModel(), 0.5);
-	}
-
-	private static Model createModel() {
-		final var idleImages = new String[8];
-
-		for (var i = 0; i < 8; i++)
-			idleImages[i] = String.format("sprites/player/Male_%s_Idle0.png", i);
-
-		final var walkingAnimation = new IsometricImage[10];
-		final var images = new String[8];
-
-		for (var frame = 0; frame < 10; frame++) {
-			for (var i = 0; i < 8; i++)
-				images[i] = String.format("sprites/player/Male_%s_Run%s.png", i, frame);
-
-			walkingAnimation[frame] = new IsometricImage(images);
-		}
-
-		return new Model(new IsometricImage(idleImages), new Animation(walkingAnimation, 7));
+		super(Model.createModel("sprites/player/Male"), 0.5);
 	}
 }
